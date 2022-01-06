359b52d3af62e9741b88c2483c2434198dbeb7d2
殷胜浩 <shenghao.yin@eslink.com>
2022-01-06 10:48:46 +0800

    update: 调整项目目录

--- 脚手架自带文件/src/index.js
+++ 脚手架自带文件/src/index.js
@@ -5,13 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  // 检查子组件中的语法
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 记录页面性能
 reportWebVitals();
